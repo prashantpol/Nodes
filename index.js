5dc103fee06f9d2b4627d200baabae407d6c61b9
Prashant Pol <prashantn.pol@Prashants-MacBook-Air.local>
2016-09-19 21:21:33 -0400

    seneca and pm2 installed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+var http = require('http');
+var server = http.createServer(function (request, response) {
+response.writeHead(200, {"Content-Type": "text/plain"});
+response.end("Hello World\n");
+});
+server.listen(8000, '127.0.0.1');
+console.log('Server is listening on port number 8000 :: 127.0.0.1:8000');
+
+
+ fs = require('fs') 
+fs.readFile('package.json', 'utf8', function (err,data) { 
+if (err) { 
+return console.log(err); 
+} 
+console.log(data); 
+});
