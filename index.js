ae860f99471ec495d89d080617b5675998e24c47
Prashant Pol <prashantn.pol@Prashants-MacBook-Air.local>
2016-09-19 21:39:12 -0400

    ignore file updated

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ response.end("Hello World\n");
 server.listen(8000, '127.0.0.1');
 console.log('Server is listening on port number 8000 :: 127.0.0.1:8000');
 
+var test=77;
+
+for(var i=0;i<3;i++){
+    console.log(i);
+}
 
  fs = require('fs') 
 fs.readFile('package.json', 'utf8', function (err,data) { 
